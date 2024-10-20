74c92d03e93b2674e763189f2cabbfba5e1435fb
unknown <you@example.com>
2024-10-20 17:56:21 +0300

    adding subscrib commponent

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 import TopProducts from './components/TopProducts/TopProducts'
 import Banner from './components/Banner/Banner'
+import Subscrib from './components/Subscrib/Subscrib'
 
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
       <Products/>
       <TopProducts/>
       <Banner/>
+      <Subscrib/>
     </div>
   )
 }
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import BannerImg from '../../assets/women/women2.png'
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
 
 
 const Banner = () => {
@@ -9,10 +12,35 @@ const Banner = () => {
             <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 items-center'>
                 {/* image section  */}
                 <div>
-                    <img src={BannerImg} alt="" />
+                    <img src={BannerImg} alt="" className='max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_12px_12px_rgba(0,0,0,1)] object-cover' />
                 </div>
                 {/* text detail section  */}
-
+                <div className='flex flex-col gap-4 justify-center sm:pt-0 '>
+                  <h1 data-aos="fade-up" className='text-3xl sm:text-4xl font-bold'>
+                  Winter Sale upto 50% Off
+                  </h1>
+                  <p data-aos="fade-up" className='text-sm text-gray-500 traking-widest leading-5'>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio
+                  </p>
+                  <div className='flex flex-col gap-4'>
+                    <div data-aos="fade-up" className='flex gap-4 items-center'>
+                    <GrSecure className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-violet-100 dark:bg-violet-400' />
+                    <p>Quality Products</p>
+                    </div>
+                    <div data-aos="fade-up" className='flex gap-4 items-center'>
+                    <IoFastFood className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-orange-100 dark:bg-orange-400' />
+                    <p>Fast Delivery</p>
+                    </div>
+                    <div data-aos="fade-up" className='flex gap-4 items-center'>
+                    <GiFoodTruck className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-green-100 dark:bg-green-400' />
+                    <p>Easy Payment method</p>
+                    </div>
+                    <div data-aos="fade-up" className='flex gap-4 items-center'>
+                    <GiFoodTruck className='text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-yellow-100 dark:bg-yellow-400' />
+                    <p>Get Offers</p>
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
